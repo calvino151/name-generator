96cc00946650d4725abc8305a0039eae3cf87518
calvino151 <calvinmaxwell@hotmail.com>
2014-12-15 22:15:00 -0500

    code

---
+++ Code/ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Name Generator");
+            Console.WriteLine("Would you like a guy or girl name");
+            string input = Console.ReadLine();
+            if (input == "guy")
+            {
+                string[] lines = File.ReadAllLines("guys.txt");
+                Random rand = new Random();
+                Console.WriteLine("your randomly chosen name is: " + lines[rand.Next(lines.Length)]);
+                Console.ReadLine();
+            }
+            else if (input == "girl")
+            {
+                string[] lines = File.ReadAllLines("girls.txt");
+                Random rand = new Random();
+                Console.WriteLine("your randomly chosen name is: " + lines[rand.Next(lines.Length)]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
